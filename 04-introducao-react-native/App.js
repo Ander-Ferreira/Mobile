6bce0a23bf03c9bb770877b66e410a0b927167cd
Ander-Ferreira <andersonadm1314@gmail.com>
2025-03-18 20:32:29 -0300

    commitando segunda parte da aula 04

--- 04-introducao-react-native/App.js
+++ 04-introducao-react-native/App.js
@@ -1,9 +1,11 @@
 //Imports
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, ScrollView } from 'react-native';
 
 //Função que compõe os componentes
 //Retorna o que vai ser renderizado na tela (Template feito JSX)
+
+const nome = 'Anderson'
 export default function App() {
 
 //Lógica do meu componente
@@ -14,14 +16,31 @@ function alerta(){
 
 //Retorno com o jsx
 return (
+  
+  // Scroll view permite que o conteúdo vá até depois da barra de rolagem
+  //Não pode ser usado sozinho, tem que ter uma view dentro
+  //Ele só enviar o conteúdo, precisa de ter algo dentro, exemplo, a View
+  
+    <ScrollView>
     <View style={styles.container}>
+    
       <Text>Hello World! APP PRA</Text>
     
       <Text>{2+2}</Text> 
-    
+
+      <Text>{nome}</Text> 
+
+      <Text style={styles.textoGrande}>
+        {/*Aqui peguei um style personalizado que criei lá em baixo*/}
+        Estilo Personalizado
+        </Text> 
+
+      {/* Botão sempre precisa de um título */}
+      
       <Button title='Clicar' onPress={alerta}></Button>
 
-      <StatusBar style="auto"/>
+      {/* <StatusBar style="auto"/> */}
+      <Text style={{fontSize:50, fontStyle:'italic'}}>Algum texto</Text>
 
       <Image source={{
         uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/1200px-A_small_cup_of_coffee.JPG"
@@ -30,8 +49,18 @@ return (
         height: 400,
         width: 400
       }}
+      
+      />
+
+<Image source={require('./imagens/moto.jpg')}
+      style={{
+        height: 400,
+        width: 400
+      }}
+      
       />
     </View>
+    </ScrollView>
   );
 }
 
@@ -42,4 +71,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textoGrande:{
+    fontSize:40,
+    color:'#000'
+  }
 });
